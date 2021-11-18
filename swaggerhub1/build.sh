1d9bfacddd91241a845f32b8d8ecca07d5ff3352
nksugumaran16 <naveen@gmail.com>
2021-11-18 10:25:00 -0600

    Added jar deploy command

--- swaggerhub1/build.sh
+++ swaggerhub1/build.sh
@@ -5,3 +5,5 @@ java -jar openapi-generator-cli-5.2.0.jar generate -g spring -i openapi.yaml -c 
 cd spring-boot-codegenerator
 
 mvn clean test compile package
+
+curl -v --user admin:password --data-binary @spring-boot-codegenerator/target/spring-boot-codegenerator-1.0.0.jar -X PUT "http://192.168.1.67:8082/artifactory/artifactsjar"
