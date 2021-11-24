740af601464b06fe1bdc35f78b48e5e76be7dff2
nksugumaran16 <55332000+nksugumaran16@users.noreply.github.com>
2021-11-24 11:14:18 -0600

    removed version hard coding

--- swaggerhub1/build.sh
+++ swaggerhub1/build.sh
@@ -2,6 +2,8 @@
 
 #Shell script to build jar file
 
+version=$(sed -n 's/^ *"version" *: *//p' openapi.yaml | grep -oP '"\K[^"]+')
+
 java -jar openapi-generator-cli-5.2.0.jar generate -g spring -i openapi.yaml -c config.json -o spring-boot-codegenerator
 
 cd spring-boot-codegenerator
@@ -11,4 +13,4 @@ mvn clean test compile package
 
 #rl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-1.0.0.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-1.0.0.jar'
 
-curl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-2.0.2.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-2.0.2.jar'
+curl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-2.0.2.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-$version.jar'
