b88e9d29879edc371db8971c2482e7100479f0b9
nksugumaran16 <55332000+nksugumaran16@users.noreply.github.com>
2021-11-24 11:17:17 -0600

    Update build.sh

--- swaggerhub1/build.sh
+++ swaggerhub1/build.sh
@@ -13,4 +13,4 @@ mvn clean test compile package
 
 #rl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-1.0.0.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-1.0.0.jar'
 
-curl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-2.0.2.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-$version.jar'
+curl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-2.0.2.jar "http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-$version.jar"
