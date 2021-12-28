07c695ae1a3d6e789c306904636784045223238e
nksugumaran16 <55332000+nksugumaran16@users.noreply.github.com>
2021-12-27 23:22:45 -0600

    Update build.sh

--- swaggerhub1/build.sh
+++ swaggerhub1/build.sh
@@ -21,19 +21,3 @@ mvn clean test compile package
 #rl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-1.0.0.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-1.0.0.jar'
 
 curl -vSf -u "admin:password" -X PUT -T "target/spring-boot-codegenerator-$version.jar" "http://192.168.1.67:8082/artifactory/artifactsjar-generic-local/spring-boot-codegenerator-$version.jar"
-
-cd ..
-
-mkdir $version
-
-cp openapi.yaml $version/
-
-git branch
-
-git checkout -b openapi$version
-
-git add .
-
-git commit -m "Addded new $version openapi file"
-
-git push https://nksugumaran16:naveenpriya2020@github.com/nksugumaran16/Demo-Integration.git --all
