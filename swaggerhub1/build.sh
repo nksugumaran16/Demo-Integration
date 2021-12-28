3aae0250cfffa2534ee100bf141fd23082a105b3
nksugumaran16 <55332000+nksugumaran16@users.noreply.github.com>
2021-12-27 22:24:45 -0600

    Update build.sh

--- swaggerhub1/build.sh
+++ swaggerhub1/build.sh
@@ -4,15 +4,14 @@
 
 version=$(sed -n 's/^ *"version" *: *//p' openapi.yaml | grep -oP '"\K[^"]+')
 
-mkdir $version
+
 
 #sudo npm install @openapitools/openapi-generator-cli -g
 #sudo openapi-generator-cli version-manager set 5.3.0
 #sudo npm install @openapitools/openapi-generator-cli -D
 
-cp openapi.yaml $version/
 
-openapi-generator-cli generate -g spring -i $version/openapi.yaml -c config.json -o spring-boot-codegenerator
+openapi-generator-cli generate -g spring -i openapi.yaml -c config.json -o spring-boot-codegenerator
 
 cd spring-boot-codegenerator
 
@@ -22,3 +21,15 @@ mvn clean test compile package
 #rl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-1.0.0.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-1.0.0.jar'
 
 curl -vSf -u "admin:password" -X PUT -T "target/spring-boot-codegenerator-$version.jar" "http://192.168.1.67:8082/artifactory/artifactsjar-generic-local/spring-boot-codegenerator-$version.jar"
+
+mkdir $version
+
+cp openapi.yaml $version/
+
+git branch
+
+git add .
+
+git commit -m "Addded new $version openapi file"
+
+git push -u origin master
