93bf3b10aed97cb33f261e453839f9db88b58b5d
nksugumaran16 <55332000+nksugumaran16@users.noreply.github.com>
2022-01-18 09:43:50 -0600

    added maven deploy command

--- swaggerhub1/build.sh
+++ swaggerhub1/build.sh
@@ -17,7 +17,8 @@ cd spring-boot-codegenerator
 
 mvn clean test compile package
 
+mvn clean deploy
 
 #rl -vSf -u "admin:password" -X PUT -T target/spring-boot-codegenerator-1.0.0.jar 'http://192.168.1.67:8082/artifactory/artifactsjar/spring-boot-codegenerator-1.0.0.jar'
 
-curl -vSf -u "admin:password" -X PUT -T "target/spring-boot-codegenerator-$version.jar" "http://192.168.1.67:8082/artifactory/artifactsjar-generic-local/spring-boot-codegenerator-$version.jar"
+#curl -vSf -u "admin:password" -X PUT -T "target/spring-boot-codegenerator-$version.jar" "http://192.168.1.67:8082/artifactory/artifactsjar-generic-local/spring-boot-codegenerator-$version.jar"
